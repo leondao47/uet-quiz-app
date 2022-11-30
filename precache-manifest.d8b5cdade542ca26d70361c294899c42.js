b4d16bb0d065d10207909c9e500f0166c202048d
leondao47 <luyendd47@gmail.com>
2022-11-30 10:50:56 +0700

    Updates

---
+++ precache-manifest.d8b5cdade542ca26d70361c294899c42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2adf433c353ff48a1178cef0e404195",
+    "url": "/uet-quiz-app/index.html"
+  },
+  {
+    "revision": "4c39bd6839f2466dc37b",
+    "url": "/uet-quiz-app/static/css/main.f0c7afa6.chunk.css"
+  },
+  {
+    "revision": "cdd5aeda64ef1989146a",
+    "url": "/uet-quiz-app/static/js/2.0011326f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/uet-quiz-app/static/js/2.0011326f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c39bd6839f2466dc37b",
+    "url": "/uet-quiz-app/static/js/main.aa8a3be6.chunk.js"
+  },
+  {
+    "revision": "d5533177c6d9a39ff26e",
+    "url": "/uet-quiz-app/static/js/runtime-main.d5adca57.js"
+  }
+]);
